0a9d12c6a5f097ec7f2607eacea608a98c9e5385
vassu-somi <96058494+vassu-somi@users.noreply.github.com>
2022-04-08 14:04:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//import App from './App';
+function App() {
+  return (
+    <div id="root">
+      <p>I am learning React. My life is getting better</p>
+    </div>
+  );
+}
+export default App;
+ReactDOM.render(<p />, document.getElementById("root"));
